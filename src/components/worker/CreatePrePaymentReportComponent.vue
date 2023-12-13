891a71d9433ab7ee29711a19aff98a656ddf48db
nevels <neevels@mail.ru>
2023-12-14 01:35:33 +0300

    version++

--- src/components/worker/CreatePrePaymentReportComponent.vue
+++ src/components/worker/CreatePrePaymentReportComponent.vue
@@ -30,73 +30,77 @@
               </div>
               <!-- /.card-header -->
               <!-- form start -->
-              <form @submit.prevent="submitForm">
-                <div class="card-body">
-                  <div class="form-group">
-                    <div>
-                      <label for="datepicker">Авансовый отчёт от:</label>
-                      <input type="date" v-model="date" class="form-control">
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="form-group">
-                      <label for="inputCity">Наименование организации</label>
-                      <input type="text" class="form-control" id="inputCity"
-                        placeholder="Наименование организации" v-model="organization" required>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="form-group">
-                      <label for="inputOrganization">Бухгалтер</label>
-                      <select class="form-control" name="accountant" v-model="accountant">
-                        <option v-for="user in managerUsers" :value="user.id">{{ user.name }}</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="form-group">
-                      <label for="inputCity">Назначение аванса</label>
-                      <input type="text" class="form-control" id="inputCity"
-                        placeholder="Назначение аванса" v-model="prePaymentReportGoal" required>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="form-group">
-                      <label for="inputCity">Полученный аванс</label>
-                      <input type="number" min="1" class="form-control" id="inputCity"
-                        placeholder="Полученный аванс" v-model="prePaymentSum" required>
-                    </div>
-                  </div>
-                </div>
+<!--              <form @submit.prevent="submitForm">-->
+<!--                <div class="card-body">-->
+<!--                  <div class="form-group">-->
+<!--                    <div>-->
+<!--                      <label for="datepicker">Авансовый отчёт от:</label>-->
+<!--                      <input type="date" v-model="date" class="form-control">-->
+<!--                    </div>-->
+<!--                  </div>-->
+<!--                  <div class="form-group">-->
+<!--                    <div class="form-group">-->
+<!--                      <label for="inputCity">Наименование организации</label>-->
+<!--                      <input type="text" class="form-control" id="inputCity"-->
+<!--                        placeholder="Наименование организации" v-model="organization" required>-->
+<!--                    </div>-->
+<!--                  </div>-->
+<!--                  <div class="form-group">-->
+<!--                    <div class="form-group">-->
+<!--                      <label for="inputOrganization">Бухгалтер</label>-->
+<!--                      <select class="form-control" name="accountant" v-model="accountant">-->
+<!--                        <option v-for="user in managerUsers" :value="user.id">{{ user.name }}</option>-->
+<!--                      </select>-->
+<!--                    </div>-->
+<!--                  </div>-->
+<!--                  <div class="form-group">-->
+<!--                    <div class="form-group">-->
+<!--                      <label for="inputCity">Назначение аванса</label>-->
+<!--                      <input type="text" class="form-control" id="inputCity"-->
+<!--                        placeholder="Назначение аванса" v-model="prePaymentReportGoal" required>-->
+<!--                    </div>-->
+<!--                  </div>-->
+<!--                  <div class="form-group">-->
+<!--                    <div class="form-group">-->
+<!--                      <label for="inputCity">Полученный аванс</label>-->
+<!--                      <input type="number" min="1" class="form-control" id="inputCity"-->
+<!--                        placeholder="Полученный аванс" v-model="prePaymentSum" required>-->
+<!--                    </div>-->
+<!--                  </div>-->
+<!--                </div>-->
 
-                <div>
-                    <a href="#" @click.prevent="addNewExpenseField" style="margin-right: 300px;">
-                      <i class="fa fa-plus fa-2x"></i>
-                    </a>
-                    <a href="#" @click.prevent="removeExpenseField">
-                        <i class="fa fa-times fa-2x"></i>
-                    </a>
-                  </div>
+<!--                <div>-->
+<!--                    <a href="#" @click.prevent="addNewExpenseField" style="margin-right: 300px;">-->
+<!--                      <i class="fa fa-plus fa-2x"></i>-->
+<!--                    </a>-->
+<!--                    <a href="#" @click.prevent="removeExpenseField">-->
+<!--                        <i class="fa fa-times fa-2x"></i>-->
+<!--                    </a>-->
+<!--                  </div>-->
 
-                <div v-for="(expense, index) in expensesRequestList" :key="index">
-                    <input type="date" class="form-control" v-model="expense.date"
-                      placeholder="Дата">
-                    <input type="number" class="form-control" v-model="expense.number"
-                      placeholder="Номер" min="1">
-                    <input type="text" class="form-control" v-model="expense.name"
-                      placeholder="Наименование документа (расхода)">
-                    <input type="number" class="form-control" v-model="expense.sum"
-                      placeholder="Сумма" min="1">
-                    <br>
-                    <br>
-                    
-                  </div>
-                <!-- /.card-body -->
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Сохранить</button>
-                </div>
-              </form>
-
+<!--                <div v-for="(expense, index) in expensesRequestList" :key="index">-->
+<!--                    <input type="date" class="form-control" v-model="expense.date"-->
+<!--                      placeholder="Дата">-->
+<!--                    <input type="number" class="form-control" v-model="expense.number"-->
+<!--                      placeholder="Номер" min="1">-->
+<!--                    <input type="text" class="form-control" v-model="expense.name"-->
+<!--                      placeholder="Наименование документа (расхода)">-->
+<!--                    <input type="number" class="form-control" v-model="expense.sum"-->
+<!--                      placeholder="Сумма" min="1">-->
+<!--                    <br>-->
+<!--                    <br>-->
+<!--                    -->
+<!--                  </div>-->
+<!--                &lt;!&ndash; /.card-body &ndash;&gt;-->
+<!--                <div class="card-footer">-->
+<!--                  <button type="submit" class="btn btn-primary">Сохранить</button>-->
+<!--                </div>-->
+<!--              </form>-->
+                <form @submit.prevent="submitForm" enctype="multipart/form-data">
+                  <input type="text" v-model="formData.name" placeholder="Введите имя">
+                  <input type="file" @change="handleFileChange" multiple>
+                  <button type="submit">Отправить</button>
+                </form>
               <div v-if="success != ''" class="alert alert-success alert-dismissible">
                   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
                   <h5><i class="icon fas fa-check"></i>Авансовый отчёт был успешно создан</h5>
@@ -137,10 +141,17 @@ import '@fortawesome/fontawesome-free/js/all.js';
         ],
         showAdditionalFields: false,
         success: '',
-        users: []
+        users: [],
+        formData: {
+          name: '',
+          files: []
+        }
       };
     },
     methods: {
+      handleFileChange(event) {
+        this.formData.files = event.target.files;
+      },
       addNewExpenseField() {
         this.expensesRequestList.push({ date: '', number: '', name: '', sum: null });
       },
@@ -160,37 +171,69 @@ import '@fortawesome/fontawesome-free/js/all.js';
         };
 
         const token = localStorage.getItem('token')
+        const id = 1;
 
-        // Отправляем данные формы на сервер
-        axios.post('http://localhost:8400/trip', data,
-        {
+        const formData = new FormData();
+        formData.append('fileNames', JSON.stringify(["first", "second"]));
+        for (let file of this.formData.files) {
+          formData.append('files', file);
+        }
+        const expenseReportRequest = {
+          managerId: 123, // Замените на реальное значение
+          adminId: 456, // Замените на реальное значение
+          companyName: 'Example Company', // Замените на реальное значение
+          goalOfReport: 'Some goal', // Замените на реальное значение
+          amountOfMoney: 1000, // Замените на реальное значение
+          infoList: [
+            { expenseType: 'Type1', amount: 200 },
+            { expenseType: 'Type2', amount: 300 },
+            // Добавьте другие объекты ExpenseInfo по мере необходимости
+          ]
+        }
+        formData.append("body", JSON.stringify(expenseReportRequest));
+        console.log(formData)
+        axios.post('http://localhost:8400/expense-report/' + id, formData, {
           headers: {
-            //'Accept': 'application/json',
-            'Content-Type': 'application/json',
+            // 'Content-Type': 'multipart/form-data',
             'Authorization': `Bearer ${token}`
           }
-        })
-          .then(response => {
-            if (response.data.message) {
-              this.error = response.data.message; // Заменяем текущий массив сообщений новым сообщением
-              this.success = ''; // Очищаем успешное сообщение
-            } else {
-              this.date = ''
-              this.organization = ''
-              this.admin = ''
-              this.accountant = ''
-              this.prePaymentReportGoal = ''
-              this.prePaymentSum = ''
-              this.expensesRequestList = []
-              this.success = 'Авансовый отчёт был успешно добавлен'
-            }
-          })
-          .catch(error => {
-            if (error.response.status === 400) {
-              this.error = error.response.data.message; // Заменяем текущий массив ошибок новым массивом ошибок
-              this.success = ''; // Очищаем успешное сообщение
-            }
-          });
+        }).then(response => {
+          // Обработка успешного ответа
+        }).catch(error => {
+          // Обработка ошибки
+        });
+
+
+        // Отправляем данные формы на сервер
+        // axios.post('http://localhost:8400/trip', data,
+        // {
+        //   headers: {
+        //     'Accept': 'application/json',
+            // 'Content-Type': 'application/json',
+            // 'Authorization': `Bearer ${token}`
+          // }
+        // })
+        //   .then(response => {
+        //     if (response.data.message) {
+        //       this.error = response.data.message; // Заменяем текущий массив сообщений новым сообщением
+        //       this.success = ''; // Очищаем успешное сообщение
+        //     } else {
+        //       this.date = ''
+        //       this.organization = ''
+        //       this.admin = ''
+        //       this.accountant = ''
+        //       this.prePaymentReportGoal = ''
+        //       this.prePaymentSum = ''
+        //       this.expensesRequestList = []
+        //       this.success = 'Авансовый отчёт был успешно добавлен'
+        //     }
+        //   })
+        //   .catch(error => {
+        //     if (error.response.status === 400) {
+        //       this.error = error.response.data.message; // Заменяем текущий массив ошибок новым массивом ошибок
+        //       this.success = ''; // Очищаем успешное сообщение
+        //     }
+        //   });
       },
       
       logout() {
