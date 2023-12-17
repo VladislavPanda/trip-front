787458d81fbefc232c710885f0f49b0c0537062e
VladislavPanda <pepartvlad7@gmail.com>
2023-12-17 20:20:56 +0300

    - request reject reason

--- src/components/accountant/BidsTaken.vue
+++ src/components/accountant/BidsTaken.vue
@@ -105,6 +105,31 @@
         </div>
       </section>
     </div>
+    <div class="modal fade" id="rejectModal" tabindex="-1" role="dialog" aria-labelledby="rejectModalLabel"
+       aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="rejectModalLabel">Отклонение заявки</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="form-group">
+              <label for="reason">Причина отклонения</label>
+              <input type="text" class="form-control" id="reason" v-model="rejectReason">
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+          <button type="button" class="btn btn-primary" @click="submitReject">Отклонить</button>
+        </div>
+      </div>
+    </div>
+  </div>
   </template>
   
   <script>
@@ -116,6 +141,7 @@
       return {
         requests: [],
         file: '',
+        rejectReason: '',
         showAdditionalInfo: {}
       };
     },
@@ -200,7 +226,35 @@
           }
         );
       },
-      reject(requestId) {
+    reject(requestId) {
+      this.requestId = requestId;
+      $('#rejectModal').modal('show');
+    },
+    submitReject(requestId) {
+      const token = localStorage.getItem('token')
+      axios.post('http://localhost:8400/trip/' + this.requestId,
+          {
+            status: 'Отклонена',
+            description: this.rejectReason
+          },
+          {
+            headers: {
+              'Accept': 'application/json',
+              'Authorization': `Bearer ${token}`
+            }
+          }).then(response => {
+        if (response.data.message) {
+          this.error = response.data.message; // Заменяем текущий массив сообщений новым сообщением
+        } else {
+          window.location.reload();
+        }
+      }).catch(error => {
+            // Обработка ошибок
+            console.error(error);
+          }
+      )
+    },
+      /*reject(requestId) {
         const token = localStorage.getItem('token')
 
         axios.post('http://localhost:8400/trip/' + requestId, {
@@ -222,7 +276,7 @@
             console.error(error);
           }
         )
-      }, 
+      }, */
       close(requestId) {
         const token = localStorage.getItem('token')
         
